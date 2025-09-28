20b8624951bc5223d9328b0d136d3bf9653f7608
Jan Hendrickx <jan@janhendrickx.be>
2025-09-28 14:06:24 +0200

    feat: :sparkles: Init front end

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,37 @@
 const { src, dest, watch, series, parallel } = require('gulp');
-const sass = require('gulp-dart-sass'); // Gebruik gulp-dart-sass om de Legacy API te vermijden
+const sass = require('gulp-dart-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
-const path = require('path');
+const sourcemaps = require('gulp-sourcemaps');
 
-// // Pad naar je SSL-certificaat en sleutel (pas deze paden aan naar je eigen certificaten)
-// const certPath = path.join('/Applications/ServBay/ssl/private/tls-certs/eskobar.local', 'eskobar.local.crt');
-// const keyPath = path.join('/Applications/ServBay/ssl/private/tls-certs/eskobar.local', 'eskobar.local.key');
 
-// Compile Sass naar CSS
 function compileSass() {
-    const sass = require('gulp-dart-sass');
-    console.log('Loaded gulp-dart-sass version:', sass.compiler.info);
-    
+    const sassOptions = {
+        includePaths: ['node_modules']
+    };
 
     return src('web/assets/sass/theme.scss')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.init())
+        .pipe(sass(sassOptions).on('error', sass.logError))
         .pipe(postcss([autoprefixer()]))
         .pipe(concat('style.css'))
-        .pipe(dest('web/assets/css'))
-        .pipe(browserSync.stream());
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('web/assets/css'));
 }
 
-// Minify CSS
 function minifyCSS() {
-    return src('web/assets/css/style.css', { allowEmpty: true })
-        .pipe(cleanCSS({ compatibility: 'ie8' }).on('error', (err) => {
-            console.error('Error in clean-css:', err.message);
-        }))
+    return src('web/assets/css/style.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(concat('style.min.css'))
         .pipe(dest('web/assets/css'));
 }
 
-// Watch voor wijzigingen
 function watchFiles() {
-    watch('web/assets/sass/**/*.scss', series(compileSass, minifyCSS)); // Genereert style.css en min.css
-    watch('web/assets/css/style.min.css').on('change', browserSync.reload); // Alleen minified bestand monitoren
+    watch('web/assets/sass/**/*.scss', series(compileSass, minifyCSS));
 }
 
-// Default task
-exports.default = series(
-    parallel(compileSass, minifyCSS),
-    watchFiles
-);
+exports.sass = series(compileSass, minifyCSS);
+exports.watch = series(compileSass, minifyCSS, watchFiles);
+exports.default = exports.watch;
