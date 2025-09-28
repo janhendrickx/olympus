1055ec4f57ea25d9bd4b3cc8f05832e47f89b98a
Jan Hendrickx <jan@janhendrickx.be>
2025-09-28 17:58:49 +0200

    chore: :sparkles: Translations

---
+++ translations/en/site.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'Door: {author}, {function}' => '<strong>By: </strong> {author}, {function}',
+];
---
+++ translations/nl/site.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'Leestijd: {readtime}min' => '<strong>Leestijd</strong>: {readtime}min',
+    'Door: {author}, {function}' => '<strong>Door</strong>: {author}, {function}',
+];
